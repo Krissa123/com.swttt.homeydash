217976d4eab23bbf7cff291727a0066a99210ba2
Kriss_Official <kristoffer@henriksson.no>
2018-01-03 04:47:10 +0100

    Revert "Homey cloud (#29)"
    This reverts commit c662805194d36dc792cd7221b0bee58fa91d5ad2.

--- api.js
+++ api.js
@@ -1,28 +1,52 @@
 module.exports = [
 
-  {
-    description: 'Save new settings',
-    method: 'POST',
-    path: '/savesettings',
-    fn: function(callback, args) {
-      var result = Homey.app.saveNewSettings(args.body);
+    {
+        description: 'Start server',
+        method: 'POST',
+        path: '/start',
+        fn: function(callback, args) {
+            var result = Homey.app.startServer();
 
-      // callback follows ( err, result )
-      callback(null, 'Saved settings');
+            // callback follows ( err, result )
+            callback(null, 'Started server');
 
-      // access /?foo=bar as args.query.foo
+            // access /?foo=bar as args.query.foo
+        }
+    }, {
+        description: 'Stop server',
+        method: 'POST',
+        path: '/stop',
+        fn: function(callback, args) {
+            var result = Homey.app.stopServer();
+
+            // callback follows ( err, result )
+            callback(null, 'Stopped server');
+
+            // access /?foo=bar as args.query.foo
+        }
+    }, {
+        description: 'Save new settings',
+        method: 'POST',
+        path: '/savesettings',
+        fn: function(callback, args) {
+            var result = Homey.app.saveNewSettings(args.body);
+
+            // callback follows ( err, result )
+            callback(null, 'Saved settings');
+
+            // access /?foo=bar as args.query.foo
+        }
+    }, {
+        description: 'Get current local ip',
+        method: 'GET',
+        path: '/currentip',
+        fn: function(callback, args) {
+            var result = Homey.app.currentIp();
+
+            // callback follows ( err, result )
+            callback(null, result);
+
+            // access /?foo=bar as args.query.foo
+        }
     }
-  }, {
-    description: 'Get config.json',
-    method: 'GET',
-    path: '/config.json',
-    fn: function(callback, args) {
-      var result = Homey.app.getConfig();
-
-      // callback follows ( err, result )
-      callback(null, result);
-
-      // access /?foo=bar as args.query.foo
-    }
-  }
 ]
